335ea2197e88056502a1f15ac8385414eaba33f4
qxyslqh <14661915+qxyslqh@user.noreply.gitee.com>
2024-09-13 18:04:19 +0800

    feat: 添加路径检验

--- 3122004868/paper-check/src/main/java/org/example/Main.java
+++ 3122004868/paper-check/src/main/java/org/example/Main.java
@@ -26,23 +26,32 @@ public class Main {
         String targetURL = args[2];
 
         // 校验参数是否为空字符串
-        if (isString(originTextURL)) {
+        if (isPathString(originTextURL)) {
             System.err.println("错误: 论文原文路径需要是一个合法的字符串。");
             return false;
         }
-        if (isString(originAddTextURL)) {
+        if (isPathString(originAddTextURL)) {
             System.err.println("错误: 抄袭版论文路径需要是一个合法的字符串。");
             return false;
         }
-        if (isString(targetURL)) {
+        if (isPathString(targetURL)) {
             System.err.println("错误: 输出的答案文件路径需要是一个合法的字符串。");
             return false;
         }
         return true;
     }
     // 检查字符串是否为合法，是否为null或者为空
-    private static boolean isString(String str) {
-        return StringUtils.isBlank(str);
+    private static boolean isPathString(String str) {
+        return !StringUtils.isBlank(str) && isPath(str);
     }
-
+    private static boolean isPath(String path) {
+        File file = new File(path);
+        try {
+            // 尝试获取绝对路径，检查路径格式是否正确
+            file.getCanonicalPath();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
